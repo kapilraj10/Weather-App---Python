2c459e0ddf0ac1a5bb10ae362eb7686a1e1dc513
kapil <kapilrajkc10@gmail.com>
2024-11-21 22:49:56 +0545

    Update weathera.py

--- weathera.py
+++ weathera.py
@@ -1,18 +1,18 @@
 import requests
 
 def get_weather(city):
-    # Replace with your API key
+    
     api_key = '836e7753abfaea941cca25c329b4de9e'
-    # Correct API endpoint for current weather
+    
     base_url = "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}"
     
-    # Parameters for the API request
+    
     params = {"q": city, "appid": api_key, "units": "metric"}
 
-    # Make the API request
+    
     response = requests.get(base_url, params=params)
 
-    # Check if the response is valid
+    
     if response.status_code == 200:
         data = response.json()
         weather = {
